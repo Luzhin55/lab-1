e5db003bcb49d0ab19641c4223b9e9fb7e67c360
Luzhin55 <superdimon25@gmail.com>
2023-11-27 08:27:02 +0300

    нова гілка

---
+++ source/repos/Лабораторна робота 1/Лабораторна робота 1/новая ветка.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Лабораторна_робота_1
+{
+    internal class новая_ветка
+    {
+    }
+}
